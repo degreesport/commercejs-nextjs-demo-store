bc44f16de040fa3f501da53a1963897facf2a8fe
degreesport <62086684+degreesport@users.noreply.github.com>
2020-11-16 15:39:39 -0500

    Replaced Hbanner images. added copy.
    see title

--- components/homepage/HomeBanner.js
+++ components/homepage/HomeBanner.js
@@ -8,12 +8,12 @@ export default function HomeBanner() {
         className="font-size-display1 mt-5 text-center mx-auto text-uppercase"
         style={{ maxWidth: '53rem' }}
       >
-        "A picture is a poem without words"-Eamon Miller.
+    Be it a business or an event, your logo has a profound impact on how you are publicly received. You will be shocked at how simply refreshing your brand identity will bring back enthusiasm to your team.
       </p>
       <div className="d-flex align-items-center justify-content-center mt-3 mb-5">
         <Link href="/about">
           <a className="d-flex py-3 align-items-center font-color-black borderbottom border-color-black">
-            <p className="mr-3">Find out more</p>
+            <p className="mr-3">LETS TALK BUSINESS</p>
             <img src="/icon/arrow-long-right.svg" />
           </a>
         </Link>
